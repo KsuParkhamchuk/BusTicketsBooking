c2da7e2e96f65d706f5ae5aa392693e235e86cdd
Oleg Kudin <oleg.kudin@hiendsys.com>
2020-09-27 19:40:25 +0300

    add home attributes

--- src/main/java/com/ticketsbooking/trip/TripController.java
+++ src/main/java/com/ticketsbooking/trip/TripController.java
@@ -1,7 +1,9 @@
 package com.ticketsbooking.trip;
 
 import com.ticketsbooking.user.User;
+import com.ticketsbooking.userDetails.UserDetailsImpl;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TripController {
 
-    @GetMapping("/home")
-    public String getHomePage(@AuthenticationPrincipal User user, Model model) {
-        model.addAttribute("user", user);
+    @GetMapping({"/home","/"})
+    public String getHomePage(@AuthenticationPrincipal UserDetailsImpl userImpl, Model model) {
+        model.addAttribute("user", userImpl != null ? userImpl.getUser() : null);
         return "home";
     }
 }
