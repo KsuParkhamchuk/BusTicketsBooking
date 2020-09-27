89e78c6798f3f738597f610e318fa13a46c3dd4a
Oleg Kudin <oleg.kudin@hiendsys.com>
2020-09-27 16:42:51 +0300

    git init

---
+++ src/main/java/com/ticketsbooking/TicketsBookingApplication.java
@@ -0,0 +1,13 @@
+package com.ticketsbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticketsbooking/TicketsBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketsbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
